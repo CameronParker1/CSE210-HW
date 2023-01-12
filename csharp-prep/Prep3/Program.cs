dae18ef5f7f8d9ec7df7d60aa81e1e0e573fd075
CameronParker1 <par18060@byui.edu>
2023-01-11 18:03:32 -0700

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        //Console.WriteLine("What is the magic number? ");
+        //string number = Console.ReadLine();
+        //int magicNumber = int.Parse(number);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); 
+
+        //Console.WriteLine("What is your guess? ");
+        //string guess = Console.ReadLine();
+        //int userGuess = int.Parse(guess);
+
+        int userGuess = -1;
+        
+        while (userGuess != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+            
+            if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
